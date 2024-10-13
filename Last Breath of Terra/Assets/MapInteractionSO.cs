8a1418382fbfa0cab5e3f1e6db482e427752e040
Meongjeong <258xsw@naver.com>
2024-10-13 23:09:36 +0900

    feat : Add function to disable gravity on wall collision

---
+++ Last Breath of Terra/Assets/MapInteractionSO.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "MapInteraction", menuName = "ScriptableObject")]
+
+public class MapInteractionSO : ScriptableObject
+{
+    public float gravityOffDuration;
+
+    public void UseSimulated(Rigidbody2D rb)
+    {
+        rb.simulated = true;
+    }
+    public void StopUsingSimulated(Rigidbody2D rb)
+    {
+        rb.simulated = false;
+    }
+}

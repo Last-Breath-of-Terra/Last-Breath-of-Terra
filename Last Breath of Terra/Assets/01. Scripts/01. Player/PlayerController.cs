e32e6cb051c3ffb09db21815cc7184cc3b55ac28
hyeona <90515062+BaeHyeonA@users.noreply.github.com>
2024-10-15 16:22:03 +0900

    feat: Add acceleration to player movement
    - 플레이어 이동에 가속화 추가
    - 특정 초동안 움직일 경우 가속이 붙으며, 최대 속도가 제한되어 있음

--- Last Breath of Terra/Assets/01. Scripts/01. Player/PlayerController.cs
+++ Last Breath of Terra/Assets/01. Scripts/01. Player/PlayerController.cs
@@ -9,21 +9,29 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 1f;
+    public float baseSpeed = 1f;
+    public float maxSpeed = 5f;
+    public float accelerationTime = 3f;
 
     private Rigidbody2D rb;
     private Animator _animator;
     private Vector2 moveDirection;
+
+    [SerializeField] private float currentSpeed;
+    private float accelerationTimer;
 
 
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
         _animator = this.GetComponent<Animator>();
+
+        currentSpeed = baseSpeed;
     }
 
     void Update()
     {
+        HandleAcceleration();
     }
 
     void FixedUpdate()
@@ -33,7 +41,21 @@ public class PlayerController : MonoBehaviour
 
     void Move()
     {
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, rb.velocity.y);
+        rb.velocity = new Vector2(moveDirection.x * currentSpeed, rb.velocity.y);
+    }
+
+    void HandleAcceleration()
+    {
+        if (moveDirection != Vector2.zero)
+        {
+            accelerationTimer += Time.deltaTime;
+            currentSpeed = Mathf.Lerp(baseSpeed, maxSpeed, accelerationTimer / accelerationTime);
+        }
+        else
+        {
+            accelerationTimer = 0f;
+            currentSpeed = baseSpeed;
+        }
     }
 
     #region InpuySystem

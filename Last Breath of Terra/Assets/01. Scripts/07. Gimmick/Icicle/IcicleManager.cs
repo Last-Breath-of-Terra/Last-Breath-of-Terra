02f210d36eb6c9145d2c35eca9c6631c8d6a8ae2
Meongjeong <258xsw@naver.com>
2025-07-29 14:10:21 +0900

    feat : 돌기믹 추가
    - 돌기믹 : 넉백 + 속도 저하
    - IcicleManager 통해서 스테이지1에 배치하시면 됩니다.

--- Last Breath of Terra/Assets/01. Scripts/07. Gimmick/Icicle/IcicleManager.cs
+++ Last Breath of Terra/Assets/01. Scripts/07. Gimmick/Icicle/IcicleManager.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class IcicleManager : Singleton<IcicleManager>
 {
     public GameObject IciclePrefab;
-    public string poolName;
     public float delayTimer;
+    public string poolName;
+
     void Start()
     {
+        poolName = IciclePrefab.name;
         PoolManager.Instance.CreatePool(poolName, IciclePrefab, gameObject.transform);
     }
 
---
+++ Last Breath of Terra/Assets/01. Scripts/07. Gimmick/Stone.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.InputSystem;
+
+public class Stone : MonoBehaviour
+{
+    public float moveDisableDuration = 1f;
+    public float knockbackForce = 10f;
+    public float slowDownDuration = 5f;
+
+    private float fallSpeed;
+
+    [SerializeField] private float fallDuration = 1f;
+    [SerializeField] private float maxFallSpeed = 20f;
+    private Sequence seq;
+
+
+    private void Start()
+    {
+        fallSpeed = 0f;
+
+        seq = DOTween.Sequence();
+        transform.DOShakeRotation(
+            duration: 0.3f,
+            strength: 15f,
+            vibrato: 10,
+            randomness: 90f,
+            fadeOut: false
+        );
+        seq.AppendInterval(0.2f).AppendCallback(() => FallStone());
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PlayerController player = other.GetComponent<PlayerController>();
+            PlayerMovement playerMovement = other.GetComponent<PlayerMovement>();
+            if (player == null || playerMovement == null) return;
+
+            //이동 속도 변화
+            if (seq != null && seq.IsActive())
+            {
+                seq.Kill();
+                player.SetCanMove(true);
+                playerMovement.SpeedChangeRate = 1f;
+            }
+            player.SetCanMove(false);
+            Vector2 knockbackDir = (player.transform.position - transform.position).normalized;
+            Rigidbody2D rb = player.Rb;
+            rb.velocity = Vector2.zero;
+            rb.AddForce(knockbackDir * knockbackForce, ForceMode2D.Impulse);
+
+            seq = DOTween.Sequence();
+            seq.AppendInterval(moveDisableDuration).AppendCallback(() =>
+                {
+                    player.SetCanMove(true);
+                    Debug.Log("Can move");
+                    playerMovement.SpeedChangeRate = 0.5f;
+                })
+                .AppendInterval(slowDownDuration).AppendCallback(() => playerMovement.SpeedChangeRate = 1f);
+        }
+
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("충돌 : " + other.gameObject.name);
+            PoolManager.Instance.ReturnObject(IcicleManager.Instance.poolName, gameObject);
+        }
+    }
+
+    public void FallStone()
+    {
+        DOTween.To(() => fallSpeed, x => fallSpeed = x, maxFallSpeed, fallDuration)
+            .SetEase(Ease.InQuad);
+    }
+
+    IEnumerator ChangeMoveSpeed(GameObject player)
+    {
+        yield return new WaitForSeconds(moveDisableDuration);
+        Debug.Log("속도가 안돌아오는 이유가 뭘까");
+        player.GetComponent<PlayerController>().SetCanMove(true);
+        player.GetComponent<PlayerMovement>().SpeedChangeRate = 1f;
+    }
+}

6d7671bf3ff55338dbb02364e3f0e43d795e2614
Meongjeong <258xsw@naver.com>
2025-10-29 22:20:19 +0900

    feat : 바람 사운드 추가

--- Last Breath of Terra/Assets/01. Scripts/07. Gimmick/Wind/Wind.cs
+++ Last Breath of Terra/Assets/01. Scripts/07. Gimmick/Wind/Wind.cs
@@ -15,7 +15,15 @@ public class Wind : MonoBehaviour
     private Coroutine coroutine;
     private bool isToRightOfWind;
     private WindManager.WindDirection playerDirection;
-    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            WindManager.Instance.PlayWindAudio(gameObject);
+        }
+    }
+
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
